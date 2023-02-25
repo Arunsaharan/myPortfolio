e4fd93474c60440ec9f5bd38ac0468cbcd699af0
Arun <saharan.arun1124@gmail.com>
2023-02-25 20:06:37 +0530

    fourth commit - done with destop version

--- index.js
+++ index.js
@@ -4,6 +4,58 @@ let loader = document.getElementById('pageLoader');
 
 window.addEventListener('load', () => loader.style.display = 'none');
 
+// sticky navbar on scroll
+
+const target = document.getElementById('organisation');
+
+const addClassSticky = function() {
+  let navBar = document.getElementsByClassName('top-navbar');
+  for (var i = 0; i < navBar.length; i++) {
+    navBar[i].className += ' stickyNav';
+  }
+}
+
+const addStickyOnIntersection = (entries) => {
+  if (entries[0].isIntersecting) {
+    addClassSticky();
+  }
+  observer.unObserve(target);
+}
+
+const observer = new IntersectionObserver(addStickyOnIntersection, {
+  threshold: 1
+});
+
+observer.observe(target);
+
+
+
+// lazy loading website content
+
+
+function reveal() {
+  let revealItem = document.querySelectorAll('.reveal');
+
+  for (var i = 0; i < revealItem.length; i++) {
+
+    let windowHeight = window.innerHeight;
+    let revealTop = revealItem[i].getBoundingClientRect().top;
+    let revealPoint = 70;
+
+    if (revealTop < windowHeight - revealPoint) {
+      revealItem[i].classList.add('active');
+    } else {
+      revealItem[i].classList.remove('active')
+    }
+
+  }
+}
+
+
+window.addEventListener('scroll', reveal);
+
+
+
 
 // typed js animation
 const typed = document.querySelector('.typed');
@@ -24,22 +76,14 @@ if (typed) {
 // swiper js
 
 var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 2,
-      spaceBetween: 10,
-      autoplay:{
-        delay:2500,
-        disableOnInteraction:false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-
-
-// lazy loading on scroll
-
-const content = document.querySelector('.lazycontent');
-window.addEventListener('scroll', () => {
-  content.style.bottom = `${window.scrollY}px`;
+  slidesPerView: 2,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
