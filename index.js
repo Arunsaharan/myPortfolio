d98775adb427f620a5f2d02bd2c79b74e9b2c903
Arun <saharan.arun1124@gmail.com>
2023-04-11 16:42:47 +0530

    added new image and updated some sections

--- index.js
+++ index.js
@@ -1,100 +1,95 @@
 // alert('working')
+// page loader animation
 
-let loader = document.getElementById('pageLoader');
+let loader = document.getElementById("pageLoader");
+let myPageLoader = () => (loader.style.display = "none");
+window.addEventListener("load", () => setTimeout(myPageLoader, 2500));
 
-window.addEventListener('load', () => loader.style.display = 'none');
+// active links
+
+const activeLoacation = window.location.href;
+console.log(activeLoacation);
 
 // sticky navbar on scroll
 
-const target1 = document.getElementById('organisation');
-const target2 = document.getElementById('resumeBox');
+const target1 = document.getElementById("organisation");
+const target2 = document.getElementById("resumeBox");
 
-const addClassSticky = function() {
-  let navBar = document.getElementsByClassName('top-navbar');
+const addClassSticky = function () {
+  let navBar = document.getElementsByClassName("top-navbar");
   for (var i = 0; i < navBar.length; i++) {
-    navBar[i].classList.add('stickyNav') ;
+    navBar[i].classList.add("stickyNav");
   }
-    // observer1.unobserve(target1);
-}
+};
 
-const removeClassSticky = function(){
-  let navBar = document.getElementsByClassName('top-navbar');
+const removeClassSticky = function () {
+  let navBar = document.getElementsByClassName("top-navbar");
   for (var i = 0; i < navBar.length; i++) {
-    navBar[i].classList.remove('stickyNav');
+    navBar[i].classList.remove("stickyNav");
   }
-}
+};
 
 const addStickyOnIntersection = (entries) => {
   if (entries[0].isIntersecting) {
     addClassSticky();
   }
-}
+};
 
 const removeStickyOnIntersection = (entries) => {
   if (entries[0].isIntersecting) {
     removeClassSticky();
   }
-}
+};
 
 const observer1 = new IntersectionObserver(addStickyOnIntersection, {
-  threshold: 0
+  threshold: 0,
 });
 const observer2 = new IntersectionObserver(removeStickyOnIntersection, {
-  threshold: 0
+  threshold: 0,
 });
 
 observer2.observe(target2);
 observer1.observe(target1);
 
-
-
 // lazy loading website content
 
-
 function reveal() {
-  let revealItem = document.querySelectorAll('.reveal');
+  let revealItem = document.querySelectorAll(".reveal");
 
   for (var i = 0; i < revealItem.length; i++) {
-
     let windowHeight = window.innerHeight;
     let revealTop = revealItem[i].getBoundingClientRect().top;
     let revealPoint = 70;
 
     if (revealTop < windowHeight - revealPoint) {
-      revealItem[i].classList.add('active');
+      revealItem[i].classList.add("active");
     } else {
-      revealItem[i].classList.remove('active')
+      revealItem[i].classList.remove("active");
     }
-
   }
 }
 
-
-window.addEventListener('scroll', reveal);
-
-
-
+window.addEventListener("scroll", reveal);
 
 // typed js animation
-const typed = document.querySelector('.typed');
+const typed = document.querySelector(".typed");
 
 if (typed) {
-  let typed_strings = typed.getAttribute('data-typed-items');
-  typed_strings = typed_strings.split(',');
-  new Typed('.typed', {
+  let typed_strings = typed.getAttribute("data-typed-items");
+  typed_strings = typed_strings.split(",");
+  new Typed(".typed", {
     strings: typed_strings,
     loop: true,
     typeSpeed: 50,
     backSpeed: 50,
-    backDelay: 2000
+    backDelay: 2000,
   });
 }
-
 
 // swiper js
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   spaceBetween: 10,
   autoplay: {
     delay: 2500,
@@ -105,3 +100,12 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+// // tilt js
+
+// VanillaTilt.init(document.querySelector("#my-element"), {
+//   max: 25,
+//   speed: 400,
+//   glare: true,
+//   "max-glare": 0.5,
+// });
